857593d9ad69f3397b07e1b61878a7cc70d95806
Bappidiu <bappi15-4900@diu.edu.bd>
2025-06-17 04:01:46 +0600

    docInfo

--- src/pages/Appoinment.jsx
+++ src/pages/Appoinment.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appoinment = () => {
+
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+
+  const [ docInfo,setDocInfo ] = useState([])
+
+  const fetchDocInfo = async () =>{
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo)
+    console.log(docInfo.image)
+  }
+
+  useEffect(()=>{
+    fetchDocInfo()
+  },[doctors,docId])
+
   return (
     <div>
-        
+        {/**--------------doctor details--------- */}
+        <div>
+          <div>
+              <img src={docInfo.image} alt="" />
+          </div>
+        </div>
     </div>
   )
 }
